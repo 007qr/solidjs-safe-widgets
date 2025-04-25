5ea3996811ef0fd4ce7715c59384fdf53c3ad3b1
007qr <aayushpatil558321@gmail.com>
2025-04-26 00:00:28 +0530

    added animations for input box screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { Accessor, For } from "solid-js";
+import { Accessor, createSignal, For, Show } from "solid-js";
 import Logo from "./components/icons/Logo";
+import { Motion, Presence } from "solid-motionone";
+
+export type Flow = "email" | "name" | "phone" | "otp" | "done";
 
 export default function App() {
     const firstLine = "Hey👋 You're new here.";
     const secondLine = "Let's get you setup.";
+    const flowPattern = ["name", "email", "phone", "otp", "done"];
+    const [flow, setFlow] = createSignal<number>(0);
 
     return (
         <>
@@ -18,18 +23,21 @@ export default function App() {
                     <For each={firstLine.split(" ")}>
                         {(item: string, index: Accessor<number>) => (
                             <>
-                            <span
-                                class={`staggered-animation whitespace-pre inline-block animate-fade-in ${item === 'new' ? 'font-dot-gothic font-medium': ''}`}
-
-                                style={{
-                                    "animation-delay": `${
-                                        (index() + 1) * 200
-                                    }ms`,
-                                }}
-                            >
-                                {item}
-                            </span>
-                            <span
+                                <span
+                                    class={`staggered-animation whitespace-pre inline-block animate-fade-in ${
+                                        item === "new"
+                                            ? "font-dot-gothic font-medium"
+                                            : ""
+                                    }`}
+                                    style={{
+                                        "animation-delay": `${
+                                            (index() + 1) * 200
+                                        }ms`,
+                                    }}
+                                >
+                                    {item}
+                                </span>
+                                <span
                                     class="whitespace-pre inline-block animate-fade-in"
                                     style={{
                                         "animation-delay": `${
@@ -43,22 +51,25 @@ export default function App() {
                             </>
                         )}
                     </For>
-                    <br/>
+                    <br />
                     <For each={secondLine.split(" ")}>
                         {(item: string, index: Accessor<number>) => (
                             <>
-                            <span
-                                class={`staggered-animation whitespace-pre inline-block animate-fade-in ${item === 'setup.' ? 'font-dot-gothic font-medium': ''}`}
-
-                                style={{
-                                    "animation-delay": `${
-                                        (index() + 5) * 200
-                                    }ms`,
-                                }}
-                            >
-                                {item}
-                            </span>
-                            <span
+                                <span
+                                    class={`staggered-animation whitespace-pre inline-block animate-fade-in ${
+                                        item === "setup."
+                                            ? "font-dot-gothic font-medium"
+                                            : ""
+                                    }`}
+                                    style={{
+                                        "animation-delay": `${
+                                            (index() + 5) * 200
+                                        }ms`,
+                                    }}
+                                >
+                                    {item}
+                                </span>
+                                <span
                                     class="whitespace-pre inline-block animate-fade-in"
                                     style={{
                                         "animation-delay": `${
@@ -139,20 +150,204 @@ export default function App() {
 
                 <div class="bg-white mt-[152px] w-full h-[340px] rounded-[32px] p-[32px] flex justify-between">
                     <div class="flex flex-col gap-[16px]">
-                        <h4 class="text-[#333232] text-[21px] leading-[120%] tracking-[-2%] font-medium font-instrument-sans">
-                            Enter your name
-                        </h4>
-                        <div class="bg-[#F5F5F5] flex flex-col gap-0.5 justify-center p-[12px] rounded-[16px] w-[374px] h-[70px]">
-                            <p class="text-[#6B6B6B] text-[13px] font-inter">
-                                Your name
-                            </p>
-                            <input
-                                type="text"
-                                class="font-inter text-[17px] outline-none border-none text-[##1D1D1F]"
-                                placeholder="type here"
-                                value="Vish Vadlamani"
+                        <Presence exitBeforeEnter>
+                            <Show when={flowPattern[flow()] === "name"}>
+                                <Motion.h4
+                                    initial={{ opacity: 0, y: 30 }}
+                                    animate={{
+                                        opacity: 1,
+                                        y: 0,
+                                        transition: {
+                                            delay: 0.01,
+                                            easing: [0.19, 1, 0.22, 1],
+                                            duration: 1.2,
+                                        },
+                                    }}
+                                    exit={{
+                                        opacity: 0,
+                                        scale: 1.01,
+                                        y: -30,
+                                        transition: {
+                                            duration: 0.7,
+                                            easing: [0.16, 1, 0.29, 0.99],
+                                        },
+                                    }}
+                                    class="text-[#333232] text-[21px] leading-[120%] tracking-[-2%] font-medium font-instrument-sans"
+                                >
+                                    Enter your name
+                                </Motion.h4>
+                            </Show>
+
+                            <Show when={flowPattern[flow()] === "email"}>
+                                <Motion.h4
+                                    initial={{ opacity: 0, y: 30 }}
+                                    animate={{
+                                        opacity: 1,
+                                        y: 0,
+                                        transition: {
+                                            delay: 0.01,
+                                            easing: [0.19, 1, 0.22, 1],
+                                            duration: 1.2,
+                                        },
+                                    }}
+                                    exit={{
+                                        opacity: 0,
+                                        scale: 1.01,
+                                        y: -30,
+                                        transition: {
+                                            duration: 0.7,
+                                            easing: [0.16, 1, 0.29, 0.99],
+                                        },
+                                    }}
+                                    class="text-[#333232] text-[21px] leading-[120%] tracking-[-2%] font-medium font-instrument-sans"
+                                >
+                                    Enter your email
+                                </Motion.h4>
+                            </Show>
+
+                            <Show when={flowPattern[flow()] === "phone"}>
+                                <Motion.h4
+                                    initial={{ opacity: 0, y: 30 }}
+                                    animate={{
+                                        opacity: 1,
+                                        y: 0,
+                                        transition: {
+                                            delay: 0.01,
+                                            easing: [0.19, 1, 0.22, 1],
+                                            duration: 1.2,
+                                        },
+                                    }}
+                                    exit={{
+                                        opacity: 0,
+                                        scale: 1.01,
+                                        y: -30,
+                                        transition: {
+                                            duration: 0.7,
+                                            easing: [0.16, 1, 0.29, 0.99],
+                                        },
+                                    }}
+                                    class="text-[#333232] text-[21px] leading-[120%] tracking-[-2%] font-medium font-instrument-sans"
+                                >
+                                    Enter your phone
+                                </Motion.h4>
+                            </Show>
+                        </Presence>
+
+                        <Presence exitBeforeEnter>
+                            <Show when={flowPattern[flow()] === "name"}>
+                                <Motion.div
+                                    initial={{ opacity: 0, y: 30 }}
+                                    animate={{
+                                        opacity: 1,
+                                        y: 0,
+                                        transition: {
+                                            delay: 0.01,
+                                            easing: [0.19, 1, 0.22, 1],
+                                            duration: 1.2,
+                                        },
+                                    }}
+                                    exit={{
+                                        opacity: 0,
+                                        scale: 1.01,
+                                        y: -30,
+                                        transition: {
+                                            duration: 0.7,
+                                            easing: [0.16, 1, 0.29, 0.99],
+                                        },
+                                    }}
+                                    class="bg-[#F5F5F5] flex flex-col gap-0.5 justify-center p-[12px] rounded-[16px] w-[374px] h-[70px]"
+                                >
+                                    <p class="text-[#6B6B6B] text-[13px] font-inter">
+                                        Your name
+                                    </p>
+                                    <input
+                                        type="text"
+                                        class="font-inter text-[17px] outline-none border-none text-[##1D1D1F]"
+                                        placeholder="type here"
+                                        value="Vish Vadlamani"
+                                    />
+                                </Motion.div>
+                            </Show>
+
+                            <Show when={flowPattern[flow()] === "email"}>
+                                <Motion.div
+                                    initial={{ opacity: 0, y: 30 }}
+                                    animate={{
+                                        opacity: 1,
+                                        y: 0,
+                                        transition: {
+                                            delay: 0.01,
+                                            easing: [0.19, 1, 0.22, 1],
+                                            duration: 1.2,
+                                        },
+                                    }}
+                                    exit={{
+                                        opacity: 0,
+                                        scale: 1.01,
+                                        y: -30,
+                                        transition: {
+                                            duration: 0.7,
+                                            easing: [0.16, 1, 0.29, 0.99],
+                                        },
+                                    }}
+                                    class="bg-[#F5F5F5] flex flex-col gap-0.5 justify-center p-[12px] rounded-[16px] w-[374px] h-[70px]"
+                                >
+                                    <p class="text-[#6B6B6B] text-[13px] font-inter">
+                                        Your email
+                                    </p>
+                                    <input
+                                        type="text"
+                                        class="font-inter text-[17px] outline-none border-none text-[##1D1D1F]"
+                                        placeholder="type here"
+                                    />
+                                </Motion.div>
+                            </Show>
+
+                            <Show when={flowPattern[flow()] === "phone"}>
+                                <Motion.div
+                                    initial={{ opacity: 0, y: 30 }}
+                                    animate={{
+                                        opacity: 1,
+                                        y: 0,
+                                        transition: {
+                                            delay: 0.01,
+                                            easing: [0.19, 1, 0.22, 1],
+                                            duration: 1.2,
+                                        },
+                                    }}
+                                    exit={{
+                                        opacity: 0,
+                                        scale: 1.01,
+                                        y: -30,
+                                        transition: {
+                                            duration: 0.7,
+                                            easing: [0.16, 1, 0.29, 0.99],
+                                        },
+                                    }}
+                                    class="bg-[#F5F5F5] flex flex-col gap-0.5 justify-center p-[12px] rounded-[16px] w-[374px] h-[70px]"
+                                >
+                                    <p class="text-[#6B6B6B] text-[13px] font-inter">
+                                        Your phone number
+                                    </p>
+                                    <input
+                                        type="text"
+                                        class="font-inter text-[17px] outline-none border-none text-[##1D1D1F]"
+                                        placeholder="type here"
+                                    />
+                                </Motion.div>
+                            </Show>
+                        </Presence>
+
+                        <button
+                            on:click={() => setFlow((v) => (v + 1) % flowPattern.length)}
+                            class="bg-black w-[56px] h-[56px] flex items-center justify-center rounded-full mt-auto self-end"
+                        >
+                            <img
+                                src="/arrow-right.svg"
+                                alt=""
+                                class="w-[24px] h-[24px]"
                             />
-                        </div>
+                        </button>
                     </div>
                     <div class="bg-[#f5f5f5] w-[500px] h-[276px] rounded-[24px]"></div>
                 </div>
