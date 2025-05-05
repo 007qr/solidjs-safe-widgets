a1316c791d8bbdc1a87ebb3a44e6be378ff72503
007qr <aayushpatil558321@gmail.com>
2025-05-05 12:28:01 +0530

    updated mastercard widget size

--- src/components/MasterCard.tsx
+++ src/components/MasterCard.tsx
@@ -3,11 +3,11 @@ import MasterCardLogo from "./icons/MasterCardLogo";
 export default function MasterCard({number}:{number: number}) {
     return (
         <div class="flex gap-[10px]">
-            <div class="w-[28px] h-[28px] rounded-[4px] border border-[#1D1D1F14] flex items-center justify-center overflow-hidden relative">
+            <div class="w-[32px] h-[32px] rounded-[4px] border border-[#1D1D1F14] flex items-center justify-center overflow-hidden relative">
                 <span class="absolute inset-0 flex items-center justify-center animate-mastercard-logo">
-                    <MasterCardLogo width="23.33" height="14.33" />
+                    <MasterCardLogo width="26" height="15" />
                 </span>
-                <span class="absolute inset-0 flex items-center justify-center font-inter text-[9px] leading-[130%] tracking-[0%] text-[#494949] animate-mastercard-text">
+                <span class="absolute inset-0 flex items-center justify-center font-inter text-[11px] leading-[130%] tracking-[0%] text-[#494949] animate-mastercard-text">
                     {number}
                 </span>
             </div>
