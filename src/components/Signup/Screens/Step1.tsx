4ce7141f8f3e12599b5abe62ce835e6cb9f6ddaa
007qr <aayushpatil558321@gmail.com>
2025-05-10 01:11:29 +0530

    updated gradient

--- src/components/Signup/Screens/Step1.tsx
+++ src/components/Signup/Screens/Step1.tsx
@@ -10,18 +10,33 @@ interface Props {
 export default function Step1(props: Props) {
     return (
         <>
-            <video
+            {/* <video
                 autoplay
                 muted
                 loop
                 class="absolute w-[90%] h-[90%] object-cover"
             >
                 <source src="/masked-video.webm" type="video/webm" />
-            </video>
+            </video> */}
 
-            <h1 class="[transform:translate3d(0,0,0)] rounded-[48px] max-lg:text-[48px] break-words flex items-center justify-center p-[32px] lg:p-[75px] text-[64px] absolute top-0 left-0 w-full h-full bg-white text-black font-bold leading-[110%] tracking-[-2%] mix-blend-screen">
+            <h1
+                class="max-lg:text-[48px] text-[64px] font-bold leading-[110%] tracking-[-2%] p-[32px] lg:p-[75px]  whitespace-pre-line"
+                style={{
+                    "background-image":
+                        "linear-gradient(to right, #67e8f9, #3b82f6, #1d4ed8)",
+                    "-webkit-background-clip": "text",
+                    "-moz-background-clip": "text",
+                    "background-clip": "text",
+                    color: "transparent",
+                    "-webkit-text-fill-color": "transparent",
+                    // "text-fill-color": "transparent",
+                }}
+            >
                 {props.title}
             </h1>
+            {/* <h1 class="[transform:translate3d(0,0,0)] rounded-[48px] max-lg:text-[48px] break-words flex items-center justify-center p-[32px] lg:p-[75px] text-[64px] absolute top-0 left-0 w-full h-full bg-white text-black font-bold leading-[110%] tracking-[-2%] mix-blend-screen">
+                {props.title}
+            </h1> */}
 
             {/* Button */}
             <button
